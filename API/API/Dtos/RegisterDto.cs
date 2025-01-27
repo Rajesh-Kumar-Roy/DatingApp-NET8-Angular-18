9e2962b10c056fe0d4fc120d52dc80a094574c09
Rajesh-Kumar-Roy <rajeshkumarroy01998@gmail.com>
2025-01-27 23:33:51 +0600

    End of Section 7

--- API/API/Dtos/RegisterDto.cs
+++ API/API/Dtos/RegisterDto.cs
@@ -4,7 +4,7 @@ namespace API.Dtos;
 public class RegisterDto
 {
     [Required]
-    public string Username { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
     [Required]
     [StringLength(8, MinimumLength = 4)]
     public string Password { get; set; } = string.Empty;
