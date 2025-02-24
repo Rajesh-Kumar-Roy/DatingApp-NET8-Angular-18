2d309d164d53572a397e707af294c300a1b13001
Rajesh-Kumar-Roy <rajeshkumarroy01998@gmail.com>
2025-02-25 00:00:30 +0600

    section 13 part: 156

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -5,11 +5,12 @@ import { MemberCardComponent } from '../member-card/member-card.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { UserParams } from '../../_models/userParams';
 import { AccountService } from '../../_services/account.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent,PaginationModule],
+  imports: [MemberCardComponent,PaginationModule,FormsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
@@ -17,7 +18,7 @@ export class MemberListComponent implements OnInit {
   private accountService = inject(AccountService);
   memberService= inject(MembersService);
   userParams = new UserParams(this.accountService.currentUser());
-  genderList = [{value: 'male', display: 'Males'}, {value: 'female', dispaly:'Females'}];
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display:'Females'}];
 
 
 
