8e5b7f10cadb5121a9dd7b23d0601fadf9b10e59
Rajesh-Kumar-Roy <rajeshkumarroy01998@gmail.com>
2025-03-11 23:39:48 +0600

    Section 14 part 172

---
+++ API/API/Controllers/LikesController.cs
@@ -0,0 +1,55 @@
+﻿using API.Data;
+using API.Dtos;
+using API.Entites;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class LikesController(ILikesRepository likesRepository) : BaseApiController
+    {
+        [HttpPost("{targetUserId:int}")]
+        public async Task<ActionResult> ToggleLike(int targetUserId)
+        {
+            var sourceUserId = User.GetUserId();
+
+            if (sourceUserId == targetUserId) return BadRequest("You cannot like yourself");
+
+            var existingLike = await likesRepository.GetUserLikes(sourceUserId, targetUserId);
+
+            if(existingLike == null)
+            {
+                var like = new UserLike
+                {
+                    SourceUserId = sourceUserId,
+                    TargetUserId = targetUserId
+                };
+
+                likesRepository.AddLike(like);
+            }
+            else
+            {
+                likesRepository.DeleteLike(existingLike);
+            }
+
+            if (await likesRepository.SaveChanges()) return Ok();
+            return BadRequest("Failed to update like");
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<IEnumerable<int>>> GetCurrentUserLikeIds()
+        {
+            return Ok(await likesRepository.GetCurrentUserLikeIds(User.GetUserId()));
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUserLikes(string predicate)
+        {
+            var users = await likesRepository.GetUserLikes(predicate, User.GetUserId());
+            return Ok(users);
+        }
+    }
+}
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { LikesService } from './likes.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
+  private likeService = inject(LikesService);
   currentUser = signal<User | null>(null);
   constructor() { }
 
@@ -36,6 +39,7 @@ export class AccountService {
   setCurrentUser(user: User){
     localStorage.setItem('user',JSON.stringify(user));
     this.currentUser.set(user);
+    this.likeService.getLikeIds();
   }
 
   logout(){
