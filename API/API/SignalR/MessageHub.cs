e4e4bd3117e4eb165ae49075a5e30b9da0435d3e
Rajesh-Kumar-Roy <rajeshkumarroy01998@gmail.com>
2025-04-25 17:57:18 +0600

    Section: 17 part: 226

---
+++ API/API/SignalR/MessageHub.cs
@@ -0,0 +1,69 @@
+﻿using API.Data;
+using API.Dtos;
+using API.Entites;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+    public class MessageHub(IMessageRepository messageRepository, IUserRepository userRepository, IMapper mapper): Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext?.Request.Query["user"];
+
+            if (Context.User == null || string.IsNullOrEmpty(otherUser))
+                throw new Exception("Can not join group");
+
+            var groupName = GetGroupName(Context.User.GetUserName(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await messageRepository.GetMessageThread(Context.User.GetUserName(), otherUser!);
+            await Clients.Group(groupName).SendAsync("ReceiveMessagethred", messages);
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(CreateMessageDto createMessageDto)
+        {
+            var username = Context.User?.GetUserName() ?? throw new Exception("could not get user.");
+
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                throw new HubException("you cannot message yourself");
+
+            var sender = await userRepository.GetUserByUserNameAsync(username);
+            var recipient = await userRepository.GetUserByUserNameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null || sender == null || recipient.UserName == null || sender.UserName == null) 
+                throw new HubException("Cannot send message at this time");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+            messageRepository.AddMessage(message);
+
+            if (await messageRepository.SaveAllAsync())
+            {
+                var group = GetGroupName(sender.UserName, recipient.UserName);
+                await Clients.Group(group).SendAsync("NewMessage", mapper.Map<MessageDto>(message));
+            }
+        }
+
+        private string GetGroupName(string caller, string? other)
+        {
+            var stringcompare = string.CompareOrdinal(caller, other) < 0;
+            return stringcompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+
+    }
+}
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, input, OnInit, output, ViewChild } from '@angular/core';
+import { Component, inject, input, ViewChild } from '@angular/core';
 import { MessageService } from '../../_services/message.service';
-import { Message } from '../../_models/message';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TimeagoModule } from 'ngx-timeago';
 
@@ -13,18 +12,14 @@ import { TimeagoModule } from 'ngx-timeago';
 })
 export class MemberMessagesComponent  {
   @ViewChild('messageForm') messageForm?: NgForm;
-  private messageService = inject(MessageService);
+  messageService = inject(MessageService);
   username = input.required<string>();
-  messages = input.required<Message[]>();
   messageContent = '';
-  updateMessages = output<Message>();
+
 
   sendMessage() {
-    this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
-      next: message => {
-        this.updateMessages.emit(message);
-        this.messageForm?.reset();
-      }
-    });
+    this.messageService.sendMessage(this.username(), this.messageContent).then(()=>{
+      this.messageForm?.reset();
+    })
   }
 }
